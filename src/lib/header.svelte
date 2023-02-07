51133478f143ee32a10fc5e6d51aa04dd0f3e943
Sam Stuart <sam@tcdevworld.com>
2023-02-07 14:58:36 +0000

    Add mock header for the navigation

---
+++ src/lib/header.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let title = "";
+
+    let fullTitle = title ? `TWIG - ${title}` : `TWIG`
+</script>
+
+<section class="header">
+    <div>
+        <h1 id="title">{fullTitle}</h1>
+    </div>
+    <div>
+        <a href="" class="nav">Home</a>
+        <a href="" class="nav">Set LEDs</a>
+    </div>
+</section>
+
+<style>
+    section.header {
+        background-color: aqua;
+        width: 100%;
+        height: 25px;
+    }
+</style>
