7319acf0918d7038b2fe0cb1f9e7d6ef0c55f8f9
Sam Stuart <sam@tcdevworld.com>
2023-02-07 14:58:57 +0000

    Add global layout for pages with libraries and header included

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Title from "$lib/header.svelte";
+    import { Styles } from 'sveltestrap';
+    import { Col, Container, Row } from 'sveltestrap';       
+</script>
+
+<Styles />
+
+<header>
+    <Title title="Home" />
+</header>
+
+<main>
+    <!-- Page Content -->
+
+    <Container>
+        <Row>
+          <Col style="background-color:bisque;"><slot></slot></Col>
+        </Row>
+    </Container>
+</main>
+
+<footer>
+    <p>A nice wee footer</p>
+</footer>
