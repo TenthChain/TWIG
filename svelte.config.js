e481281a10fcd61a3f0377510bad74f894cc5ff5
Sam Stuart <sam@tcdevworld.com>
2023-02-08 16:22:41 +0000

    Add lib folder reference to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,15 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	"paths": {
+		"$lib": [
+			"src/lib"
+		],
+		"$lib/*": [
+			"src/lib/*"
+		]
+	},
 };
 
 export default config;
