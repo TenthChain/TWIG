a1577ef33130a4a8b4012db1c2e970772da23f52
Sam Stuart <sam@tcdevworld.com>
2023-06-02 20:41:29 +0100

    Add stat card for displaying statistics in bs cards

---
+++ src/lib/components/statsCard.svelte
@@ -0,0 +1,24 @@
+
+<script>
+    // @ts-nocheck
+    export let value;
+    export let title;
+    export let icon;
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+
+<div class="card">
+    <div class="card-body">
+        <p class="mb-0">{title}</p>
+        {#if (value && value != 0)}
+        <h2 class="mb-0">{value}</h2>
+        {:else if  icon}
+        <h2 class="mb-0"><i class="{icon}"></i></h2>
+        {:else}
+        <h2 class="mb-0">Nothing passed...</h2>
+        {/if}
+    </div>
+</div>
