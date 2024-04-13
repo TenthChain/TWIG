468e09fde10830785ae62264385cc78450044b8a
Sam Stuart <sam@tcdevworld.com>
2024-04-13 09:38:41 +0100

    feat: Add rate limiting

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -16,6 +16,9 @@
 	let ProcessorActive = false;
 	let requestCount = 0;
 	let count = 0;
+	let keepProcessing = false;
+	let getRequestIntervalDuration = 5;
+	let requestsPerInterval = 10;
 
 	function componentToHex(c) {
 		var hex = c.toString(16);
@@ -38,60 +41,90 @@
 				.collection('users')
 				.authWithPassword('jamadmin@niraspberryjam.com', '123456890');
 			console.log('subscribing');
-			console.log(pb.authStore.isValid);
-			pb.collection('led_requests')
-				.subscribe('*', function (e) {
-					console.log('Got Request! Woo!');
-					try {
-						console.log('Got request!');
-						if (e.record['completed']) {
-							return;
-						}
+			console.log('Authstore / login', pb.authStore.isValid);
+			// pb.collection('led_requests')
+			// 	.subscribe('*', function (e) {
+			// 		console.log('Got Request! Woo!');
+			// 	})
+			// 	.catch((err) => {
+			// 		console.log(err.originalError);
+			// 		ProcessorActive = false;
+			// 	});
 
-						const data = {
-							seg: {
-								i: [
-									e.record['led_to_update'],
-									rgbToHex(e.record['r'], e.record['g'], e.record['b'])
-								]
-							}
-						};
-
-						const updated_record_data = {
-							led_to_update: e.record['let_to_update'],
-							requested_by: e.record['requested_by'],
-							r: e.record['r'],
-							g: e.record['g'],
-							b: e.record['b'],
-							brightness: e.record['brightness'],
-							completed: true
-						};
-
-						fetch(env.PUBLIC_tree_url, {
-							method: 'POST',
-							headers: {
-								Accept: 'application/json',
-								'Content-Type': 'application/json'
-							},
-							body: JSON.stringify(data)
-						})
-							.then((response) => response.json())
-							.then(async (response) => {
-								console.log('Finished request!');
-								const record = await pb
-									.collection('led_requests')
-									.update(e.record['id'], updated_record_data);
-							});
-						UpdateRequestCount();
-					} catch (error) {
-						console.log(error);
-						ProcessorActive = false;
-					}
-				})
-				.catch((err) => {
-					console.log(err.originalError);
+			let doRequestInterval = setInterval(async () => {
+				if (!keepProcessing) {
 					ProcessorActive = false;
+					return;
+				}
+				ProcessorActive = true;
+
+				console.log('doRequestInterval');
+
+				const startingGetRequestIntervalDuration = getRequestIntervalDuration;
+
+				const DurationPerRequest = getRequestIntervalDuration / requestsPerInterval;
+
+				let resultList = await pb.collection('led_requests').getList(1, requestsPerInterval, {
+					filter: 'completed=false'
 				});
+
+				console.log(resultList);
+
+				let internalInterval = setInterval(async () => {
+					console.log('internalInterval');
+
+					if (!keepProcessing) {
+						ProcessorActive = false;
+						clearInterval(internalInterval);
+						resultList = [];
+						return;
+					}
+					ProcessorActive = true;
+
+					if (resultList.items == 0) {
+						console.log('Completed list, exiting...');
+						clearInterval(internalInterval);
+					}
+
+					e = resultList.items[0];
+					if (e.record['completed']) {
+						return;
+					}
+
+					const data = {
+						seg: {
+							i: [e.record['led_to_update'], rgbToHex(e.record['r'], e.record['g'], e.record['b'])]
+						}
+					};
+
+					const updated_record_data = {
+						led_to_update: e.record['let_to_update'],
+						requested_by: e.record['requested_by'],
+						r: e.record['r'],
+						g: e.record['g'],
+						b: e.record['b'],
+						brightness: e.record['brightness'],
+						completed: true
+					};
+
+					fetch(env.PUBLIC_tree_url, {
+						method: 'POST',
+						headers: {
+							Accept: 'application/json',
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify(data)
+					})
+						.then((response) => response.json())
+						.then(async (response) => {
+							console.log('Finished request!');
+							const record = await pb
+								.collection('led_requests')
+								.update(e.record['id'], updated_record_data);
+						});
+					UpdateRequestCount();
+				}, DurationPerRequest * 1000);
+			}, getRequestIntervalDuration * 1000);
 		} catch (error) {
 			console.log('Error with subscription - ', error);
 			ProcessorActive = false;
@@ -99,7 +132,7 @@
 	}
 
 	function endProcessor() {
-		pb.collection('led_requests').unsubscribe();
+		//pb.collection('led_requests').unsubscribe();
 		ProcessorActive = false;
 	}
 
@@ -168,18 +201,55 @@
 
 <hr />
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-{#if FlushInProgress}
-	<button class="btn btn-danger btn-disabled btn-lg">
-		FLUSH DB! <div class="spinner-border" role="status" />
-	</button>
-{:else}
-	<button class="btn btn-danger btn-lg" on:click={ObliterateDB}> FLUSH DB! </button>
-{/if}
-
-<button class="btn btn-danger btn-lg" on:click={unsubscribe}> Unsubscribe </button>
+<div class="row">
+	<div class="col">
+		{#if FlushInProgress}
+			<button class="btn btn-danger btn-disabled btn-lg">
+				FLUSH DB! <div class="spinner-border" role="status" />
+			</button>
+		{:else}
+			<button class="btn btn-danger btn-lg" on:click={ObliterateDB}> FLUSH DB! </button>
+		{/if}
+	</div>
+	<div class="col">
+		<button class="btn btn-danger btn-lg" on:click={unsubscribe}> Unsubscribe </button>
+	</div>
+	<div class="col">
+		<div class="form-check">
+			<input
+				class="form-check-input"
+				type="checkbox"
+				bind:checked={keepProcessing}
+				id="flexCheckDefault"
+			/>
+			<label class="form-check-label" for="flexCheckDefault"> Continue processing? </label>
+		</div>
+	</div>
+	<div class="col">
+		<label for="exampleFormControlInput1" class="form-label">GET interval</label>
+		<input
+			bind:value={getRequestIntervalDuration}
+			type="number"
+			class="form-control"
+			id="exampleFormControlInput1"
+			min="0.1"
+			step="0.1"
+			max="100"
+		/>
+	</div>
+	<div class="col">
+		<label for="exampleFormControlInput2" class="form-label">Requests Per Interval</label>
+		<input
+			type="number"
+			bind:value={requestsPerInterval}
+			class="form-control"
+			id="exampleFormControlInput2"
+			min="1"
+			step="1"
+			max="100"
+		/>
+	</div>
+</div>
 
 <style>
 </style>
