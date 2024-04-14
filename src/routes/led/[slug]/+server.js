d55c4a7ae1c6d110cd190835d977fc8d9d278ade
Sam Stuart <sam@tcdevworld.com>
2024-04-14 23:57:24 +0100

    chore: format code

--- src/routes/led/[slug]/+server.js
+++ src/routes/led/[slug]/+server.js
@@ -20,90 +20,95 @@ URL {
 */
 
 // Import Environment variables from .env file
-import { env } from "$env/dynamic/private";
+import { env } from '$env/dynamic/private';
 
 // Import pocketbase
-import PocketBase from "pocketbase";
+import PocketBase from 'pocketbase';
 
-// Setup pocketbase client for communicating to server with. Plus auth 
+// Setup pocketbase client for communicating to server with. Plus auth
 // to prevent random users from submitting requests unless authored by SvelteKit
 const pb = new PocketBase(env.pb_url);
 const userData = await pb.collection("users").authWithPassword(env.pb_username, env.pb_username);
 
 // @ts-ignore
-export const GET = ( async ({ url }) => {
+export const GET = async ({ url, request }) => {
+	console.log('Got request!');
 
-    console.log("Got request!")
+	const isServerEnabledRecord = await pb
+		.collection('settings')
+		.getFirstListItem('setting_name="server_enabled"');
 
-    const isServerEnabledRecord = await pb.collection('settings').getFirstListItem('setting_name="server_enabled"');
+	if (!isServerEnabledRecord['value']) {
+		return new Response('Failed. Server not enabled.');
+	}
 
-    if (!isServerEnabledRecord["value"]) {
-        return new Response("Failed. Server not enabled.");
-    }
+	let led = -1;
+	let bri = -1;
+	let r = -1;
+	let g = -1;
+	let b = -1;
 
-    let led = -1;
-    let bri = -1;
-    let r = -1;
-    let g = -1;
-    let b = -1;
-    
-    const splitUrl = url.pathname.split("/")[2].split(",");
-    let responseString = `You got it! from url ${url}\n you want to alter ${splitUrl[0]}`;
+	const splitUrl = url.pathname.split('/')[2].split(',');
+	let responseString = `You got it! from url ${url}\n you want to alter ${splitUrl[0]}`;
 
-    switch (splitUrl.length) {
-        // Toggle on/off for LED
-        case 1:
-            led = splitUrl[0];
-            break;
+	switch (splitUrl.length) {
+		// Toggle on/off for LED
+		case 1:
+			led = splitUrl[0];
+			break;
 
-        // Set LED brightness
-        case 2:
-            led = splitUrl[0];
-            bri = splitUrl[1];
-            break;
-    
-        // Set LED colour
-        case 4:
-            led = splitUrl[0];
-            r = splitUrl[1];
-            g = splitUrl[2];
-            b = splitUrl[3];
-            break;
+		// Set LED brightness
+		case 2:
+			led = splitUrl[0];
+			bri = splitUrl[1];
+			break;
 
-        // Set LED brightness and colour
-        case 5:
-            led = splitUrl[0];
-            bri = splitUrl[1];
-            r = splitUrl[2];
-            g = splitUrl[3];
-            b = splitUrl[4];
-            break;
+		// Set LED colour
+		case 4:
+			led = splitUrl[0];
+			r = splitUrl[1];
+			g = splitUrl[2];
+			b = splitUrl[3];
+			break;
 
-        // ???
-        default:
-            responseString += `\n\n Really...? Only ${splitUrl.length} parameters?`;
-            break;
-    }
+		// Set LED brightness and colour
+		case 5:
+			led = splitUrl[0];
+			bri = splitUrl[1];
+			r = splitUrl[2];
+			g = splitUrl[3];
+			b = splitUrl[4];
+			break;
 
-    const data = {
-        "led_to_update": led,
-        "requested_by": url.host,
-        "r": r,
-        "g": g,
-        "b": b,
-        "brightness": bri,
-        "completed": false
-    };
+		// ???
+		default:
+			responseString += `\n\n Really...? Only ${splitUrl.length} parameters?`;
+			break;
+	}
+	//const clientAddress = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
 
-    responseString += `\n\n Final data being \nled_to_update being ${led}, \nrequested_by being ${url.host}, \nr being ${r}, \ng being ${g}, \nb being ${b}, \nbrightness being ${bri}, \ncompleted being ${false}`;
+	console.log(url);
 
-    try {
-        const newRecord = await pb.collection("led_requests").create(data);
-        responseString +=  `\n\nAnd the response from pb being created on ${newRecord["created"]}`;
-    } catch (error) {
-        responseString +=  `\n\nAnd the        ERROR        from pb being ${error}`;
-    }
+	const data = {
+		led_to_update: led,
+		requested_by: url.host,
+		r: r,
+		g: g,
+		b: b,
+		brightness: bri,
+		completed: false
+	};
 
-    return new Response(responseString);
-});
+	responseString += `\n\n Final data being \nled_to_update being ${led}, \nrequested_by being ${
+		url.host
+	}, \nr being ${r}, \ng being ${g}, \nb being ${b}, \nbrightness being ${bri}, \ncompleted being ${false}`;
 
+	try {
+		const newRecord = await pb.collection('led_requests').create(data);
+		responseString += `\n\nAnd the response from pb being created on ${newRecord['created']}`;
+	} catch (error) {
+		responseString += `\n\nAnd the        ERROR        from pb being ${error}`;
+	}
+
+	return new Response(responseString);
+};
